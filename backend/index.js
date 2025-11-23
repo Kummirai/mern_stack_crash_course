380e08f5dd8b02ef110a291c10bedab306afa0bb
Milton <ajaxmilton@hotmail.com>
2025-11-23 17:07:37 +0200

    Create route to add product to database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectionDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
@@ -11,9 +12,27 @@ app.get("/", (req, res) => {
   res.send("Server running!");
 });
 
-app.post("/", (req, res) => {});
+app.post("/", async (req, res) => {
+  const product = req.body;
 
-app.listen(PORT, () => {
-  connectionDB();
+  if (!product.name || product.price || product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all data!" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+app.listen(PORT, async () => {
+  await connectionDB();
   console.log(`Sever running on port ${PORT}`);
 });
