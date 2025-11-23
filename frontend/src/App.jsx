85a9b238c194a0781ab75cf000a31c6fec129626
Milton <ajaxmilton@hotmail.com>
2025-11-23 22:05:55 +0200

    Implement header component with all required elements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import CreateProduct from "./pages/CreateProduct";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="p-5 max-w-5xl">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/create_product" element={<CreateProduct />} />
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { FaPlus } from "react-icons/fa";
+import { CiLight } from "react-icons/ci";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className="flex items-center justify-between mx-auto">
+      <Link
+        to="/"
+        className="bg-blue-900 px-6 py-2 text-xl rounded-md text-white"
+      >
+        Product Store
+      </Link>
+      <div className="flex items-center justify-between">
+        <Link to={"/create_product"}>
+          <FaPlus className="text-white bg-blue-900 p-5 rounded-md mx-3" />
+        </Link>
+        <Link to={"/create_product"}>
+          <CiLight className="text-white bg-blue-900 p-5 rounded-md" />
+        </Link>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
