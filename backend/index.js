ba20701b1cbd6a4df9553f74102074d503cf3daa
Milton <ajaxmilton@hotmail.com>
2025-11-23 17:32:11 +0200

    Add route to delete product by id

--- backend/index.js
+++ backend/index.js
@@ -7,18 +7,27 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Server running!");
 });
 
-app.post("/", async (req, res) => {
+app.post("/api/products", async (req, res) => {
   const product = req.body;
 
-  if (!product.name || product.price || product.image) {
+  if (!product.name) {
     return res
       .status(400)
-      .json({ success: false, message: "Please provide all data!" });
+      .json({ success: false, message: "Please provide product name!" });
+  } else if (!product.price) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide product price!" });
+  } else if (!product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide product image!" });
   }
 
   const newProduct = new Product(product);
@@ -29,6 +38,19 @@ app.post("/", async (req, res) => {
   } catch (error) {
     console.error(`Error: ${error.message}`);
     res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Product.findByIdAndDelete(id);
+    res
+      .status(200)
+      .json({ success: true, message: "Product deleted successfully!" });
+  } catch (error) {
+    console.error(`Error: ${ErrorEvent.message}`);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 });
 
