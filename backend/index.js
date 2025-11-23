6c26f2acfcd20bf74b1e0608adc7e9223969f532
Milton <ajaxmilton@hotmail.com>
2025-11-23 16:28:53 +0200

    Create server for the project

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("Server running!");
+});
+
 app.listen(PORT, () => {
   console.log(`Sever running on port ${PORT}`);
 });
