209f6a6d5a75794ec2feb3f763294eb04e616f18
Milton <ajaxmilton@hotmail.com>
2025-11-23 21:42:18 +0200

    Create frontend pages with basic structure and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
-import { Button, HStack } from "@chakra-ui/react";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <HStack>
-        <Button>Click me</Button>
-        <Button>Click me</Button>
-      </HStack>
-    </>
+    <div className="p-5 max-w-5xl">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/create_product" element={<CreateProduct />} />
+      </Routes>
+    </div>
   );
 }
 
